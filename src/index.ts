d3fb69de2ac12305a51b4b56b638e09a173cf297
Hyunjoong <de0978@gmail.com>
2023-07-14 23:31:03 +0900

    solve: add 2805.js
    link: https://www.acmicpc.net/problem/2805

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-export function sayHelloWorld(world: string) {
-  return `Hello ${world}`;
-}
+import * as readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const inputs = [];
+
+rl.on("line", (line) => {
+  inputs.push(line);
+
+  if (inputs.length === 1) {
+    rl.close();
+  }
+});
+
+rl.on("close", () => {
+  process.exit();
+});
